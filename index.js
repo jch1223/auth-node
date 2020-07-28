e77bb3e2ceee2873a36502d1d5fd550da1fd4c79
jch1223 <ico1828@gmail.com>
2020-07-28 17:05:38 +0900

    create/user info create

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const bodyParser = require("body-parser");
+
+const User = require("./models/user");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 mongoose
   .connect(
@@ -19,5 +26,15 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("hello world"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`port : ${port}`));
