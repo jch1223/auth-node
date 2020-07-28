f844c36f5c737df21104e28c13fe63644d8094e2
jch1223 <ico1828@gmail.com>
2020-07-28 12:15:07 +0900

    mongoDB connected

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://jch1223:1q2w3e4r!!@cluster0.frxzl.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => res.send("hello world"));
 
 app.listen(port, () => console.log(`port : ${port}`));
