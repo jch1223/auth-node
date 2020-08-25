5cc00a78b2bb1f3c86827f4fd433ed5eb26c8cef
jch1223 <ico1828@gmail.com>
2020-08-25 16:01:26 +0900

    set/redux login

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -7,7 +7,19 @@ function LandingPage(props) {
       console.log(res);
     });
   }, []);
-  return <div>landing</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      시작페이지
+    </div>
+  );
 }
 
 export default LandingPage;
